5aebe540b1f95c576abff1e20445ddacc5e48407
xiaohp <xiaohp1993@gmail.com>
2018-12-20 23:56:13 +0800

    Loading Data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash'
 import './style.css'
 import Icon from './icon.png'
+import Data from './data.xml'
 
 function component() {
     let element = document.createElement('div')
@@ -11,8 +12,10 @@ function component() {
     // Add the image to our existing div.
     var myIcon = new Image()
     myIcon.src = Icon
-    
+
     element.appendChild(myIcon)
+
+    console.log(Data)
 
     return element
 }
