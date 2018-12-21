d1dd23674d8247e8cbbb7c087deb8beae9c7ab24
xiaohp <xiaohp1993@gmail.com>
2018-12-21 16:33:36 +0800

    Specify the Mode
    use node env

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import _ from 'lodash'
 import printMe from './print.js'
 import './styles.css'
 
+if (process.env.NODE_ENV !== 'production') {
+    console.log('Looks like we are in development mode!')
+}
+
 function component() {
     let element = document.createElement('div')
     var btn = document.createElement('button')
